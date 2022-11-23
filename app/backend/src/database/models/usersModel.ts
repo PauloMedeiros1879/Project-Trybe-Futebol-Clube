4c7c4940c860375d5fba96887ec7bb0494f11fb8
Paulo <paulo_alexandre_slipknot@hotmail.com>
2022-11-23 10:32:31 -0400

    feat: adicionando o model de users email e password

--- app/backend/src/database/models/usersModel.ts
+++ app/backend/src/database/models/usersModel.ts
@@ -1,30 +1,25 @@
-import { Model } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // declare <campo>: <tipo>;
+export default class User extends Model {
+  public email: string | undefined;
+  public password: string | undefined;
 }
 
-Example.init({
-  // ... Campos
-}, {
-  // ... Outras configs
-  underscored: true,
-  sequelize: db,
-  // modelName: 'example',
-  timestamps: false,
-});
-
-/**
-  * `Workaround` para aplicar as associations em TS: 
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-export default Example;
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
