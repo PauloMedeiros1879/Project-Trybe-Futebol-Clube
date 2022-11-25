48319cdcfe6d23131a6ac3d478ab6ea2b62c173c
Paulo <paulo_alexandre_slipknot@hotmail.com>
2022-11-25 11:10:29 -0400

    feat: adiciona a rota do tipo POST do login

---
+++ app/backend/src/routes/userR.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+import User from '../database/models/usersModel';
+import UserService from '../services/userS';
+import UserController from '../controllers/userC';
+
+require('express-async-errors');
+
+const usersRoute = express.Router();
+
+const userController = new UserController(new UserService(User));
+
+usersRoute.post('/', (req, res, next) => {
+  userController.login(req, res, next);
+});
+
+export default usersRoute;
