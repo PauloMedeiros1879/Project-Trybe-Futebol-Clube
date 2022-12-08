375dbc3750aa2ff34bdbfef6f338ec49a7b33c10
Paulo <paulo_alexandre_slipknot@hotmail.com>
2022-12-08 15:45:47 -0400

    feat: criando a model de teams - req14

---
+++ app/backend/src/database/models/teamsM.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  },
+);
