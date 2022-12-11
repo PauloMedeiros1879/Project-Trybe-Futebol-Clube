7e16dc906fd24c021670767843e94c4a2b6461fa
paulo <paulo_alexandre_slipknot@hotmail.com>
2022-12-11 12:57:18 -0400

    feat: adiciona o metodo findAll de matches na camada service

---
+++ app/backend/src/services/matchS.ts
@@ -0,0 +1,16 @@
+import TeamModel from '../database/models/teamsM';
+import MatchModel from '../database/models/matchesM';
+
+export default class MatchService {
+  constructor(private matchModel: typeof MatchModel) {}
+
+  async match(): Promise<MatchModel[]> {
+    const matches = await this.matchModel.findAll({
+      include: [
+        { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return matches;
+  }
+}
