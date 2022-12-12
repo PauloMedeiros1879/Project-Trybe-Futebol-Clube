78d22143e444c71287d69665cd90cee0aaa1eef0
paulo <paulo_alexandre_slipknot@hotmail.com>
2022-12-12 14:25:07 -0400

    feat: adiciona a rota de matches com  metodo patch em /:id/finish desenvolvendo os requisitos 24/25/26

--- app/backend/src/routes/matchR.ts
+++ app/backend/src/routes/matchR.ts
@@ -11,5 +11,5 @@ const matchesRouter = express.Router();
 
 matchesRouter.get('/', (req, res) => matches.match(req, res));
 matchesRouter.post('/', auth, (req, res) => matches.matchCreate(req, res));
-
+matchesRouter.patch('/:id/finish', (req, res) => matches.matchUpdate(req, res));
 export default matchesRouter;
