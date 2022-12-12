42255df5c43ee54fb32bc7326fe6435fcbe98059
paulo <paulo_alexandre_slipknot@hotmail.com>
2022-12-12 08:17:17 -0400

    refator: padroniza as camadas de matches

--- app/backend/src/services/matchS.ts
+++ app/backend/src/services/matchS.ts
@@ -1,27 +1,48 @@
-import TeamModel from '../database/models/teamsM';
-import MatchModel from '../database/models/matchesM';
+import TeamM from '../database/models/teamsM';
+import MatchM from '../database/models/matchesM';
 
 export default class MatchS {
-  constructor(private matchModel: typeof MatchModel) {}
+  constructor(private matchModel: typeof MatchM) {}
 
-  async match(): Promise<MatchModel[]> {
+  async match(): Promise<MatchM[]> {
     const matches = await this.matchModel.findAll({
       include: [
-        { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
-        { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+        {
+          model: TeamM,
+          as: 'teamHome',
+          attributes:
+          { exclude: ['id'] },
+        },
+        {
+          model: TeamM,
+          as: 'teamAway',
+          attributes:
+          { exclude: ['id'] },
+        },
       ],
     });
     return matches;
   }
 
-  async matchFilter(inProgress: boolean): Promise<MatchModel[]> {
+  async matchFilter(inProgress: boolean): Promise<MatchM[]> {
     const matches = await this.matchModel.findAll({
       where: { inProgress },
       include: [
-        { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
-        { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+        {
+          model: TeamM,
+          as: 'teamHome',
+          attributes:
+            { exclude: ['id'] },
+        },
+        {
+          model: TeamM,
+          as: 'teamAway',
+          attributes:
+            { exclude: ['id'] },
+        },
       ],
     });
+
     return matches;
   }
 }
