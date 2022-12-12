0df5b4864a8e2eb216f95092f64b6a18908e68b6
paulo <paulo_alexandre_slipknot@hotmail.com>
2022-12-12 10:12:34 -0400

    feat: adiciona a rota com o endpoint POST de matches com metodo create

--- app/backend/src/routes/matchR.ts
+++ app/backend/src/routes/matchR.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import Match from '../database/models/matchesM';
 import MatchService from '../services/matchS';
 import MatchController from '../controllers/matchC';
+import { auth } from '../middlewares/jwt';
 
 require('express-async-errors');
 
@@ -9,5 +10,6 @@ const matches = new MatchController(new MatchService(Match));
 const matchesRouter = express.Router();
 
 matchesRouter.get('/', (req, res) => matches.match(req, res));
+matchesRouter.post('/', auth, (req, res) => matches.matchCreate(req, res));
 
 export default matchesRouter;
